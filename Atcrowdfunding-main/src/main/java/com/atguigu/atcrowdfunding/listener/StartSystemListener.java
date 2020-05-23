3ab52e0212d320b0714838fd2a09889cbd905313
qiuxiuchang <1106663896@qq.com>
2020-05-23 12:14:24 +0800

    atcrowdfunding

---
+++ Atcrowdfunding-main/src/main/java/com/atguigu/atcrowdfunding/listener/StartSystemListener.java
@@ -0,0 +1,24 @@
+package com.atguigu.atcrowdfunding.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class StartSystemListener implements ServletContextListener {
+
+	//在服务器启动时,创建application对象时需要执行的方法.
+	public void contextInitialized(ServletContextEvent sce) {
+		//1.将项目上下文路径(request.getContextPath())放置到application域中.
+		ServletContext application = sce.getServletContext();
+		String contextPath = application.getContextPath();
+		application.setAttribute("APP_PATH", contextPath);
+		System.out.println("APP_PATH...");
+		
+		//2.
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		
+	}
+
+}
